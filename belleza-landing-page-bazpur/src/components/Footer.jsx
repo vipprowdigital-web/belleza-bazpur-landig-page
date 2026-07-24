a987948b7c8693484cc17ecd1846c717d24cf268
kumkumnema07 <kumkumnema7@gmail.com>
2026-07-24 11:34:23 +0530

    remove sign up link

--- belleza-landing-page-bazpur/src/components/Footer.jsx
+++ belleza-landing-page-bazpur/src/components/Footer.jsx
@@ -199,7 +199,7 @@ const Footer = () => {
                   href="tel:+919012360088"
                   className="text-sm text-light/70 hover:text-accent transition-colors"
                 >
-                   +91 74099 40088 
+                   +91 96909 20088 
                 </a>
               </div>
 
